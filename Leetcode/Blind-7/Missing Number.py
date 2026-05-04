473d0cb06b6e0dd0e786db0ca525caf9aa8d294c
SuvarnaSN <156342036+SuvarnaSN@users.noreply.github.com>
2026-05-03 21:45:11 -0500

    Delete Leetcode/Blind-7 directory

--- Leetcode/Blind-7/Missing Number.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int: 
-        end = len(nums)       
-        for j in range(0, end+1):
-            if j not in nums:
-                return j
